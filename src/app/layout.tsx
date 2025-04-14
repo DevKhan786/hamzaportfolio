81e0c3ef741a7e2034c487bfaa690750a2cefacb
DevVain <InsaneVain786@gmail.com>
2025-04-14 22:06:38 +0100

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Hamza Khan - Portfolio",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
